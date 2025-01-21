99e5370cb51d116f90ebc155a120404857ae4c8f
Christian <50743222+jeanRodolp@users.noreply.github.com>
2025-01-21 21:20:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-dnednzk
+```python
+import discord
+from discord.ext import commands
+
+Token de ton bot (le token est à garder privé !)
+TOKEN = 'TON_BOT_TOKEN'
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+Quand le bot est prêt
+@bot.event
+async def on_ready():
+    print(f'Nous avons connecté {bot.user}!')
+
+Commande simple pour dire "Bonjour"
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Salut! Je suis ton bot modérateur!")
